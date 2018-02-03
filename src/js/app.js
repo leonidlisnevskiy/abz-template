1df1a858fc29d209f606820f94962c5661cc9b89
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2018-02-04 01:08:27 +0200

    update layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,14 @@
 import $ from 'jquery';
-import slick from 'slick-carousel';
-import formvalidator from 'jquery-form-validator';
+// import formvalidator from 'jquery-form-validator';
 
-$('.js-btn-nav').click(function(event) {
-  event.stopPropagation();
-  $('.js-header').toggleClass('is-open');
+// $('.js-btn-nav').click(function(event) {
+//   event.stopPropagation();
+//   $('.js-header').toggleClass('is-open');
 
-  if(!$('.js-header').is(e.target) && $('.js-header').has(e.target).length === 0) {
-    $('.js-header').removeClass('is-open');
-  }
-});
+//   if(!$('.js-header').is(e.target) && $('.js-header').has(e.target).length === 0) {
+//     $('.js-header').removeClass('is-open');
+//   }
+// });
 
 $(document).ready(function() {
 
