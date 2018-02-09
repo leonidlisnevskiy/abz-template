8e71aeb97ebf843e2b120c9cb75522258623dc61
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2018-02-09 13:42:32 +0200

    add index

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
+import select2 from 'select2';
 
 svg4everybody();
 
 $(document).ready(function() {
+
+  $('js-select').select2({
+    minimumResultsForSearch: -1
+  });
 
   $('textarea, input').each(function() {
     var placeholder = $(this).attr('placeholder');
