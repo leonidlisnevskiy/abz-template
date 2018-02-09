1ccb9dda166eacaa73d130cb3b8cf0bfe8a5cc03
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2018-02-09 18:52:18 +0200

    validate images

--- src/js/app.js
+++ src/js/app.js
@@ -6,20 +6,30 @@ svg4everybody();
 
 $(document).ready(function() {
 
-  $('.js-select').select2({
-    minimumResultsForSearch: -1,
-    width: '100%',
-    height: '100%',
+  const ERROR = 'is-error';
+
+  $('.js-input-file').each((i, file) => {
+    file = $(file);
+    const field = file.find('.js-input-file-field');
+
+    field.on('change', e => {
+      if (field.get(0).files[0].size/1000000 > 5) {
+        file.addClass(ERROR);
+      } else {
+        file.removeClass(ERROR);
+      }
+    });
   });
 
-  $('textarea, input').each(function() {
-    var placeholder = $(this).attr('placeholder');
+  // -----------------------------------------------------
+  $('#description').keyup(function() {
+  	var textLength = $('#description').val().length;
+  	$('.js-counter').html(textLength);
+  });
 
-    $(this).focus(function() {
-      $(this).attr('placeholder', '');
-    });
-    $(this).blur(function() {
-      $(this).attr('placeholder', placeholder);
-    });
+  $('.js-select').select2({
+  	minimumResultsForSearch: -1,
+  	width: '100%',
+  	height: '100%',
   });
 });
